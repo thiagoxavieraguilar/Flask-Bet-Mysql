6e98b472576419e08100ec0e541213b48ab0ad1d
Thiago Xavier <thiago.xavieraguilar1@gmail.com>
2023-04-21 15:34:38 -0300

    Fixed up Docker configuration and created a mobile-responsive HTML layout.

--- Bet_tx/__init__.py
+++ Bet_tx/__init__.py
@@ -1,31 +1,28 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
-import os 
+from os import path
+import os
+from flask_login import LoginManager
 
 load_dotenv()
 # Create the SQLAlchemy database instance
 db = SQLAlchemy()
 
 DB_NAME = os.environ.get('DB_NAME') 
-
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
 
     # Set the secret key for the application
-    app.config['SECRET_KEY'] = "bet_tx"
+    app.config['SECRET_KEY'] = "blog"
+    
+    # Set the SQLAlchemy database URI
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:root@mysqldb/{DB_NAME}"
 
-    # Configure the app to connect to MySQL
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:root@127.0.0.1/{DB_NAME}"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # Initialize the database instance
     db.init_app(app)
-
-    with app.app_context():
-        # Import models here to ensure tables are created when app is run
-        from . import models
 
     # Import the views module
     from .views import views
@@ -35,17 +32,28 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
+    # Import the User and Post model
+    from .models import User,Bet
+
+    # Create the database
+    create_database(app)
+
     # Initialize the LoginManager instance
-    from flask_login import LoginManager
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
     login_manager.init_app(app)
 
     # User loader function for Flask-Login
-    from .models import User
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
 
     # Return the configured Flask application instance
     return app
+
+def create_database(app):
+    """Create the database if it does not exist."""
+    if not path.exists("blog/" + DB_NAME):
+        with app.app_context():
+            db.create_all()
+        print("Created database!")
