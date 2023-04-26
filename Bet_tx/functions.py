2b7e78bbf31f2b4b0189184a531f866b78c90f42
Thiago Xavier <thiago.xavieraguilar1@gmail.com>
2023-04-25 21:44:36 -0300

    Add docstring to functions and handle errors

--- Bet_tx/functions.py
+++ Bet_tx/functions.py
@@ -3,16 +3,23 @@ import asyncio
 import os
 from dotenv import load_dotenv
 
-
+# Load the .env file
 load_dotenv()
+
+# Get the ODDS API KEY from the .env file
 api_key = os.environ.get('ODDS_API_KEY')
+
+# Set the sport keys to soccer leagues in Brazil and UEFA Champions League
 sport_key = ['soccer_brazil_campeonato', 'soccer_uefa_champs_league']
+# Set the market to Head to Head
 market = 'h2h'
 
+# Ssynchronous function that retrieves the odds from the API
 async def get_odds():
     async with httpx.AsyncClient() as client:
         matches = []
 
+        # Loop through the sport keys to retrieve odds for each
         for sport in sport_key:
             url = f'https://api.the-odds-api.com/v3/odds/?apiKey={api_key}&sport={sport}&region=uk&mkt={market}'
             try:
@@ -22,16 +29,20 @@ async def get_odds():
                     print('request feita')
                     data = response.json()
                     if data['success']:
+                        # Loop through the matches in the data
                         for match in data['data']:
+                            # Get the home and visiting teams 
                             home_team = match['home_team']
                             visit_team = match['teams'][1]
+                            # If the home team and visit team are the same, it is likely that the order is wrong
+                            # Therefore, we switch the order of the visit and home team and mark an error in the order of the teams
                             if home_team == visit_team:
                                 visit_team = match['teams'][0]   
                                 error_ordem_team = True     
                             else:
-                                error_ordem_team = False
-                                
+                                error_ordem_team = False             
                             for site in match['sites']:
+                                # Get the  odds from matches 
                                 if site['site_key'] == 'betway':
                                     if error_ordem_team == True:
                                         odd_home_team = site['odds']['h2h'][1]
@@ -41,6 +52,7 @@ async def get_odds():
                                         odd_visit_team = site['odds']['h2h'][1]
                                     odd_empate= site['odds']['h2h'][1]
 
+                                     # Add the match data to the matches list
                                     match_data = {
                                             'home_team': home_team,
                                             'home_team_odds': odd_home_team,
--- Bet_tx/views.py
+++ Bet_tx/views.py
@@ -12,18 +12,24 @@ import schedule
 import time
 import pytz
 
+
 matches = None
 
 views = Blueprint("views", __name__)
 
-#Set locale to Pt-br
+# Set locale to Pt-br
 locale.setlocale(locale.LC_ALL, 'pt_BR.utf-8')
 
 # Fuso horário do Brasil
 tz = pytz.timezone('America/Sao_Paulo')
 
 
+# Function to update matches
 def update_matches():
+    """
+    Updates the matches variable by calling the get_odds function and 
+    assigning the result to the global variable matches.
+    """
     global matches
     matches = asyncio.run(get_odds())
     print('Odds e partidas atualizadas.')
@@ -31,6 +37,9 @@ def update_matches():
 schedule.every(1).minutes.do(update_matches)
 
 def schedule_thread():
+    """
+    Runs the schedule module to periodically update the matches variable.
+    """
     while True:
         schedule.run_pending()
         time.sleep(1)
@@ -41,10 +50,20 @@ def schedule_thread():
 @views.route("/home",  methods=['GET', 'POST'])
 @login_required
 def home():
+    """
+    Renders the home page and passes the matches and current_user objects to the template.
+
+    Matches is a list of dictionaries containing odds data for soccer matches. If matches is None, calls the 
+    get_odds function asynchronously and stores the result in matches. If matches is empty, displays an error 
+    message. Otherwise, renders the home template with the matches and current_user objects.
+    """
     global matches
     if matches is None:
         matches = asyncio.run(get_odds())
-    print(matches)
+    
+    if len(matches) == 0:
+        flash('Error com a Api.')
+        return render_template('home.html', matches=matches, user=current_user)
     return render_template('home.html', matches=matches, user=current_user)
 
 
@@ -53,58 +72,72 @@ def home():
 @views.route('/show_results',  methods=['GET', 'POST'])
 @login_required
 def show_results():
+    """
+    Processes the form data from the home page and creates a new Bet object with the data.
+    """
     if request.method == 'POST':
-        form_data = dict(request.form)
-        name = current_user.username 
-        value_bet = form_data['valor']
-        del form_data['valor']
         try:
-            value_bet = float(value_bet)
-        except:    
-            flash('Por favor, insira um valor para a aposta!')
-            return redirect(url_for('home'))
-        
-        matches_bet= []
-        odds = []
-        
-        for team, value in form_data.items():
-            odds_str, outcome = value.split(',')
-            try:
-                odds_float = float(odds_str)
-            except ValueError:
-                flash(f'Valor inválido de cotação para {team}!')
-                return redirect(url_for('home'))
-            matches_bet.append((team,outcome,odds_float))
-            odds.append(odds_float)
+            form_data = dict(request.form)
+            name = current_user.username 
+            value_bet = form_data['valor']
+            # Checking if the bet value is valid
+            if value_bet is not None:
+                del form_data['valor']
+                try:
+                    value_bet = float(value_bet)
+                except:    
+                    flash('Por favor, insira um valor válido para a aposta!')
+                    return redirect(url_for('views.home'))
+            else:
+                flash('Por favor, insira um valor para a aposta!')
+                return redirect(url_for('views.home'))
+            matches_bet= []
+            odds = []
 
-        if not matches_bet:
-            flash('Selecione os times para fazer a aposta.')
-            return redirect(url_for('home'))
-        
-        odd_final = math.prod(odds)
-        odd_final_text = f'{odd_final:_.2f}'
-        value_bet_not_format = value_bet
-        value_return_bet = odd_final * value_bet
-        return_bet = locale.currency(value_return_bet, grouping=True)
-        value_bet = locale.currency(value_bet, grouping=True)
+            # Creating a list of matches and their odds
+            for team, value in form_data.items():
+                odds_str, outcome = value.split(',')
+                try:
+                    odds_float = float(odds_str)
+                except ValueError:
+                    flash(f'Valor inválido de cotação para {team}!')
+                    return render_template('home')
+                matches_bet.append((team,outcome,odds_float))
+                odds.append(odds_float)
 
-        message_body = f"Apostas feitas:\n"
-        for match in matches_bet:
-            message_body += f"- {match[0]} x {match[1]} ({match[2]})\n"
+            # Checking if at least one match is selected
+            if not matches_bet:
+                flash('Selecione os times para fazer a aposta.')
+                return render_template('home')
+            
+            # Calculating the final odds and the possible return value
+            odd_final = math.prod(odds)
+            odd_final_text = f'{odd_final:_.2f}'
+            value_bet_not_format = value_bet
+            value_return_bet = odd_final * value_bet
+            return_bet = locale.currency(value_return_bet, grouping=True)
+            value_bet = locale.currency(value_bet, grouping=True)
 
-        now_time = datetime.datetime.now(tz)
-        now_time = now_time.strftime("%H:%M")
-        message_body += f"\nODDS: *{odd_final_text}*\nValor Apostado: *{value_bet}*\nPossível Retorno: {return_bet}.\nHorário: {now_time}.\n Nome apostado: {name}"    
-        
-        matches_bet_str = '|'.join([','.join(map(str, match)) for match in matches_bet])
+            # Creating a message to send via WhatsApp with twilio
+            message_body = f"Apostas feitas:\n"
+            for match in matches_bet:
+                message_body += f"- {match[0]} x {match[1]} ({match[2]})\n"
 
+            now_time = datetime.datetime.now(tz)
+            now_time = now_time.strftime("%H:%M")
+            message_body += f"\nODDS: *{odd_final_text}*\nValor Apostado: *{value_bet}*\nPossível Retorno: {return_bet}.\nHorário: {now_time}.\n Nome apostado: {name}"    
+            
+            matches_bet_str = '|'.join([','.join(map(str, match)) for match in matches_bet])
 
-        new_bet = Bet(matches_bet=matches_bet_str, odd_final=odd_final, value_bet=value_bet_not_format, return_bet=value_return_bet,author=current_user.id)
-        db.session.add(new_bet)
-        db.session.commit()
-        send_message(message_body)            
-        session.clear()
-        return render_template('result.html',matches_bet=matches_bet, odd_final=odd_final_text,value_bet = value_bet, return_bet=return_bet)
-    
-    return redirect(url_for("home"))
-
+            # Create a new instance of the Bet class with the given parameters
+            new_bet = Bet(matches_bet=matches_bet_str, odd_final=odd_final, value_bet=value_bet_not_format, return_bet=value_return_bet,author=current_user.id)
+            # Add the new_bet object to the database session and commit
+            db.session.add(new_bet)
+            db.session.commit()
+            # Call the function to send message with twilio 
+            send_message(message_body)            
+            session.clear()
+            return render_template('result.html',matches_bet=matches_bet, odd_final=odd_final_text,value_bet = value_bet, return_bet=return_bet)
+        except:
+            return redirect(url_for('views.home'))
+    return redirect(url_for('views.home'))
